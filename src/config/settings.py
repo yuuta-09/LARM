4d1424a8d16ac6bce549ef811e7c85baf2e011b5
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2025-05-17 23:22:14 +0900

    djangoのDATABASESの設定が間違っていたため修正

--- src/config/settings.py
+++ src/config/settings.py
@@ -76,8 +76,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("POSTGRES_DB"),
-        'USER': os.environ.get("POSTGRES_PASSWORD"),
-        'PASSWORD': os.environ.get("POSTGRES_HOST"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
         'PORT': os.environ.get("POSTGRES_PORT"),
     }
 }
