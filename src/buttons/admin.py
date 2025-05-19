cf32684fa8368d567a2b350cb03bc4175602ec46
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2025-05-19 20:12:22 +0900

    ボタン・ボタン制御のためのArmの角度のモデルの作成

--- src/buttons/admin.py
+++ src/buttons/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Button, Angle
 
-# Register your models here.
+
+class AngleInline(admin.TabularInline):
+    model = Angle
+    extra = 1  # 新規フォーム表示数
+
+
+@admin.register(Button)
+class ButtonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at', 'updated_at')
+    inlines = [AngleInline]
+
+
+@admin.register(Angle)
+class AngleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'button', 'joint_id', 'degree', 'speed', 'created_at', 'updated_at')
+    list_filter = ('button', 'joint_id')
--- src/buttons/models.py
+++ src/buttons/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Button(models.Model):
+    """
+    ボタンのモデル
+    """
+    name = models.CharField(max_length=255, verbose_name='ボタン名')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='作成日時')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='更新日時')
+
+    class Meta:
+        verbose_name = 'ボタン'
+        verbose_name_plural = 'ボタン一覧'
+
+    def __str__(self):
+        return self.name
+
+
+class Angle(models.Model):
+    """
+    ボタンの角度のモデル
+    """
+    button = models.ForeignKey(Button, on_delete=models.CASCADE, related_name='angles', verbose_name='ボタン')
+    joint_id = models.IntegerField(verbose_name='関節ID')
+    degree = models.IntegerField(verbose_name='角度')
+    speed = models.IntegerField(verbose_name='速度')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='作成日時')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='更新日時')
+
+    class Meta:
+        verbose_name = 'ボタンの角度'
+        verbose_name_plural = 'ボタンの角度一覧'
+
+    def __str__(self):
+        return f'{self.button.name} - {self.joint_id}'
